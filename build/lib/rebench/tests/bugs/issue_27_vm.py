233d28d46c64fa3a7c31cb878d2734d43c59c4b8
kyvyt0n <zhukshir@gmail.com>
2024-07-03 20:13:05 +0100

    Remove build folder from repository

--- build/lib/rebench/tests/bugs/issue_27_vm.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# simple script emulating a failing benchmark
-print("Starting Richards benchmark ...")
-print("Results are incorrect")
--- build/lib/rebench/tests/features/issue_216_vm.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-invocation_number = int(sys.argv[3])
-print("RESULT-total:    %d" % invocation_number)
